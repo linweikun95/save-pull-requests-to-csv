a5e7a31f3dcd2427db1b7219952e6623d67b9793
leo2c <z429882801@qq.com>
2020-01-30 16:15:29 +0800

    Delete unusedImports

--- src/main/java/com/github/hcsp/io/Crawler.java
+++ src/main/java/com/github/hcsp/io/Crawler.java
@@ -1,17 +1,13 @@
 package com.github.hcsp.io;
 
 import com.alibaba.fastjson.JSON;
-import com.opencsv.CSVReader;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.util.ArrayList;
