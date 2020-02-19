f3fba8eed2a11e99cdf442efd3cac811e3e05e77
Dygalo <59307187+dygalo@users.noreply.github.com>
2020-02-19 22:08:01 +0800

    pull requests as csv

--- src/main/java/com/github/hcsp/io/Crawler.java
+++ src/main/java/com/github/hcsp/io/Crawler.java
@@ -11,10 +11,10 @@ import java.util.List;
 
 public class Crawler {
     public static void main(String[] args) throws IOException {
-        savePullRequestsToCSV("golang/go",2,
-                File.createTempFile("csv", ""));
+        savePullRequestsToCSV("golang/go", 2, File.createTempFile("csv", ""));
         System.out.println(File.createTempFile("csv", ""));
     }
+
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，读取前n个Pull request并保存至csvFile指定的文件中，格式如下：
     // number,author,title
     // 12345,blindpirate,这是一个标题
@@ -33,16 +33,15 @@ public class Crawler {
 //        }
         CsvWriter csvWriter = new CsvWriter(csvFile.getPath(), ',', Charset.forName("UTF-8"));
         // 写表头
-        String[] csvHeaders = { "number", "author", "title" };
+        String[] csvHeaders = {"number", "author", "title"};
         csvWriter.writeRecord(csvHeaders);
         // 写内容
         for (int i = 0; i < n; i++) {
-            String[] csvContent = {String.valueOf(pullRequests.get(i).getNumber()),
-                    pullRequests.get(i).getUser().getName() , pullRequests.get(i).getTitle()};
+            String[] csvContent = {String.valueOf(pullRequests.get(i).getNumber()), pullRequests.get(i).getUser().getName(), pullRequests.get(i).getTitle()};
             csvWriter.writeRecord(csvContent);
         }
         csvWriter.close();
-        }
+    }
 
 //        priate static String getLine()
 
