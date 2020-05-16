523dd6f8c5584a891255d6fb71779b164e46c5e7
你的英文名 <>
2020-05-16 11:41:43 +0800

    try 2

--- src/main/java/com/github/hcsp/io/Crawler.java
+++ src/main/java/com/github/hcsp/io/Crawler.java
@@ -13,14 +13,14 @@ import java.util.List;
 
 public class Crawler {
     public static void main(String[] args) throws IOException {
-        savePullRequestsToCSV("golang/go", new File("pull.csv"));
+        savePullRequestsToCSV("golang/go",10, new File("pull.csv"));
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，读取前n个Pull request并保存至csvFile指定的文件中，格式如下：
     // number,author,title
     // 12345,blindpirate,这是一个标题
     // 12345,FrankFang,这是第二个标题
-    public static void savePullRequestsToCSV(String repo, File csvFile) throws IOException {
+    public static void savePullRequestsToCSV(String repo, int n, File csvFile) throws IOException {
         GitHub github = GitHub.connectAnonymously();
         GHRepository repository = github.getRepository(repo);
         List<GHPullRequest> pullRequests = repository.getPullRequests(GHIssueState.OPEN);
